494cad576e1cc0971236f5d83c3bc7711aa2eaa5
lbp <lbp@liuboping.com>
2018-04-01 19:18:16 +0800

    init

--- superset/config.py
+++ superset/config.py
@@ -153,19 +153,19 @@ PUBLIC_ROLE_LIKE_GAMMA = False
 # Babel config for translations
 # ---------------------------------------------------
 # Setup default language
-BABEL_DEFAULT_LOCALE = 'en'
+BABEL_DEFAULT_LOCALE = 'zh'
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = 'superset/translations'
 # The allowed translation for you app
 LANGUAGES = {
     'en': {'flag': 'us', 'name': 'English'},
-    'it': {'flag': 'it', 'name': 'Italian'},
-    'fr': {'flag': 'fr', 'name': 'French'},
+    # 'it': {'flag': 'it', 'name': 'Italian'},
+    # 'fr': {'flag': 'fr', 'name': 'French'},
     'zh': {'flag': 'cn', 'name': 'Chinese'},
-    'ja': {'flag': 'jp', 'name': 'Japanese'},
-    'de': {'flag': 'de', 'name': 'German'},
-    'pt-BR': {'flag': 'br', 'name': 'Brazilian Portuguese'},
-    'ru': {'flag': 'ru', 'name': 'Russian'},
+    # 'ja': {'flag': 'jp', 'name': 'Japanese'},
+    # 'de': {'flag': 'de', 'name': 'German'},
+    # 'pt-BR': {'flag': 'br', 'name': 'Brazilian Portuguese'},
+    # 'ru': {'flag': 'ru', 'name': 'Russian'},
 }
 # ---------------------------------------------------
 # Image and file configuration
---
+++ superset/translations/generate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+msgfmt messages.po -o messages.mo
